75c1459786d806fe1578e0baf48a1999d33392be
jaspinder <jaspinder@ucreate.co.in>
2018-11-30 15:06:18 +0530

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using dot.Models;
 
-namespace dot.Controllers
+namespace AspnetWithHeroku.Controllers
 {
     public class HomeController : Controller
     {
@@ -31,7 +29,7 @@ namespace dot.Controllers
 
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
